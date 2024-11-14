d09fc622d8bfc941f283e6a4bf7169accedd859c
darleet <darleet@mail.ru>
2024-11-14 23:43:19 +0300

    learner feat: add client

---
+++ learner/client.py
@@ -0,0 +1,25 @@
+import requests
+
+from learner.models import (EquivalenceRequest, EquivalenceResponse,
+                            MembershipRequest, MembershipResponse)
+
+
+class ClientMAT:
+    def __init__(self, host: str, port: int, secure: bool = False):
+        self.host = host
+        self.port = port
+        self.schema = "https" if secure else "http"
+
+    def post_membership(self, request: MembershipRequest) -> MembershipResponse:
+        resp = requests.post(
+            f"{self.schema}://{self.host}:{self.port}/checkWord",
+            json=request.model_dump(),
+        )
+        return MembershipResponse(**resp.json())
+
+    def post_equivalence(self, request: EquivalenceRequest) -> EquivalenceResponse:
+        resp = requests.post(
+            f"{self.schema}://{self.host}:{self.port}/checkTable",
+            json=request.model_dump(),
+        )
+        return EquivalenceResponse(**resp.json())
